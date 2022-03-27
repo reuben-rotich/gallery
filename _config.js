172e9d7c1e23793fd49d4f07ab9de559421a073d
reuben-rotich <reubenbomett@gmail.com>
2022-03-27 23:46:06 +0300

    hey

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    test: '<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    production: 'mongodb+srv://rbomett:aDm1n$TR8r@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    development: 'mongodb+srv://rbomett>:aDm1n$TR8r@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    test: 'mongodb+srv://rbomett:aDm1n$TR8r@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
 module.exports = config;
 //mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority
