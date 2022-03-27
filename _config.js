be5281d58fa6ca32281696edabdef38ca4f5ea2d
Reuben Bomett <35496372+reuben-rotich@users.noreply.github.com>
2022-03-27 20:32:18 +0300

    Add files via upload

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    test: '<rbomett>:<aDm1n$TR8r>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
 module.exports = config;
+//mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority
+// mongodb+srv://rbomett:<password>@cluster0.hdazh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
