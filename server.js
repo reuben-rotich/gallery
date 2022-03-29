96d9fe29e72b1efb3e70bf1c59c0a297ec16a6d7
reuben-rotich <reubenbomett@gmail.com>
2022-03-30 00:17:06 +0300

    added mongo uri

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-import {config} from "./_config";
+const config = require ('./_config');
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
