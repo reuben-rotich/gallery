7360ecd544c710508db2baa0baf6f6f57e414ab3
reuben-rotich <reubenbomett@gmail.com>
2022-03-30 00:04:13 +0300

    added mongo uri

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
+import {config} from "./_config";
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = config.mongoURI.production || 'mongodb://localhost/your-app-name';
+//let mongodb_url = 'mongodb://localhost:27017/';
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
